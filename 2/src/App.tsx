ab2242b4cb9695803c669195015ad91f6a9f52ee
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-04-10 15:01:55 -0300

    update layout

--- 2/src/App.tsx
+++ 2/src/App.tsx
@@ -45,55 +45,63 @@ export function App() {
   }
 
   return (
-    <main className="w-full min-h-screen px-4 py-9 flex flex-col items-center bg-black text-white">
-      <h1 className="sr-only">Perfil GitHub</h1>
+    <>
+      <main className="relative flex-1 w-full max-w-[1156px] min-h-screen px-4 py-9 flex flex-col items-center bg-black text-white lg:min-h-[537px]">
+        <h1 className="sr-only">Perfil GitHub</h1>
 
-      <img src="/project-title.png" alt="Perfil GitHub" className="mb-7" />
+        <img src="/project-title.png" alt="Perfil GitHub" className="mb-7" />
 
-      <form
-        onSubmit={handleSubmit}
-        className="flex w-full max-w-[503px] h-16 mb-8 bg-[#DDDDDD] border border-white text-black rounded-[10px]"
-      >
-        <input
-          ref={inputRef}
-          type="text"
-          name="search"
-          placeholder="Digite um usuário do Github"
-          className="px-4 placeholder:text-black focus:outline-none flex-1"
-        />
-
-        <button
-          type="submit"
-          className="h-full aspect-square flex justify-center items-center bg-primary-500 rounded-[10px] cursor-pointer"
+        <form
+          onSubmit={handleSubmit}
+          className="flex w-full max-w-[503px] h-16 mb-8 bg-[#DDDDDD] border border-white text-black rounded-[10px]"
         >
-          <img src="/magnifier.svg" alt="lupa" />
-        </button>
-      </form>
-
-      {user && !error && (
-        <div className="w-full max-w-[804px] px-8 py-4 flex flex-col sm:flex-row justify-center items-center gap-8 rounded-[25px] bg-[#D9D9D9]">
-          <img
-            src={user.avatarUrl}
-            alt={user.name + " avatar"}
-            className="w-full max-w-[220px] rounded-full border-2 border-primary-500 flex-1"
+          <input
+            ref={inputRef}
+            type="text"
+            name="search"
+            placeholder="Digite um usuário do Github"
+            className="px-4 placeholder:text-black focus:outline-none flex-1"
           />
 
-          <div className="flex-2">
-            <h2 className="mb-4 font-bold text-lg text-primary-500">{user.name}</h2>
+          <button
+            type="submit"
+            className="h-full aspect-square flex justify-center items-center bg-primary-500 rounded-[10px] cursor-pointer"
+          >
+            <img src="/magnifier.svg" alt="lupa" />
+          </button>
+        </form>
 
-            <p className="font-light text-sm text-black">{user.bio}</p>
+        {user && !error && (
+          <div className="w-full max-w-[804px] px-8 py-4 flex flex-col sm:flex-row justify-center items-center gap-8 rounded-[25px] bg-[#D9D9D9]">
+            <img
+              src={user.avatarUrl}
+              alt={user.name + " avatar"}
+              className="w-full max-w-[220px] rounded-full border-2 border-primary-500 flex-1"
+            />
+
+            <div className="flex-2">
+              <h2 className="mb-4 font-bold text-lg text-primary-500">{user.name}</h2>
+
+              <p className="font-light text-sm text-black">{user.bio}</p>
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {error && (
-        <div className="w-full max-w-[710px] p-4 flex justify-center items-center bg-[#D9D9D9] rounded-[25px]">
-          <p className="text-error-500 text-lg text-center">
-            Nenhum perfil foi encontrado com esse nome de usuário. <br />
-            Tente novamente
-          </p>
-        </div>
-      )}
-    </main>
+        {error && (
+          <div className="w-full max-w-[710px] p-4 flex justify-center items-center bg-[#D9D9D9] rounded-[25px]">
+            <p className="text-error-500 text-lg text-center">
+              Nenhum perfil foi encontrado com esse nome de usuário. <br />
+              Tente novamente
+            </p>
+          </div>
+        )}
+
+        <div className="hidden lg:block absolute top-0 right-0 w-[888px] aspect-square bg-radial from-primary-500 to-transparent to-50% translate-x-1/2 -translate-y-1/2 -z-10" />
+
+        <img src="/pattern.png" alt="" className="hidden lg:block absolute -top-[109px] -left-[71px] -z-10" />
+      </main>
+
+      <div className="hidden lg:block absolute -left-[calc(60%)] -bottom-1/2 w-[674px] aspect-square bg-radial from-primary-500 to-transparent to-50% translate-x-1/2 -translate-y-1/2 -z-10" />
+    </>
   )
 }
