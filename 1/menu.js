9aa4a0cd3b16ca891ea47a61affc28eaf5862e4f
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-04-10 15:34:55 -0300

    change desktop menu intial position

--- 1/menu.js
+++ 1/menu.js
@@ -4,15 +4,21 @@ const menuDesktop = document.querySelector("#menu");
 const menuBtnMobile = document.querySelector("#menu-button-mobile");
 const menuMobile = document.querySelector("#menu-mobile");
 
-menuBtnDesktop.addEventListener("click", () => {
+menuBtnDesktop.addEventListener("click", async () => {
   menuBtnDesktop.classList.toggle("text-primary-500");
-  const isOpen = menuDesktop.classList.toggle("translate-y-38");
+  const isOpen = !menuDesktop.classList.toggle("-top-full");
   if (isOpen) {
+    menuDesktop.classList.add("top-0");
+    menuDesktop.classList.add("translate-y-38");
     menuDesktop.classList.remove("opacity-0");
     menuBtnDesktop.querySelector("img").src = "assets/menu-desktop-active.svg";
   } else {
+    menuDesktop.classList.remove("translate-y-38");
     menuDesktop.classList.add("opacity-0");
     menuBtnDesktop.querySelector("img").src = "assets/menu-desktop.svg";
+
+    await new Promise((resolve) => setTimeout(resolve, 300));
+    menuDesktop.classList.remove("top-0");
   }
 });
 
